5669db5133820f0984da367237d564855230fd4f
petrizzo <petrizzo@gmail.com>
2023-02-22 23:09:18 -0400

    mejora del script

--- bootcamp-web.sh
+++ bootcamp-web.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Creando un contenedor que despliegue información en el navegador"
-sudo docker run -d --name bootcamp-web -p80:80 nginx
+sudo docker run -d --name bootcamp-web -p 9999:80 nginx
 sudo docker cp /home/petrizzo/Descargas/CursoDevOps/doc-devops/devops-in-tech/challenge/03-Docker/03/web/* bootcamp-web:/usr/share/nginx/html
 echo "Muestra el contenido del contenedor"
-sudo docker exec -it bootcamp-web bash
-ls /usr/share/nginx/html
+#sudo docker exec -it bootcamp-web bash
+#ls /usr/share/nginx/html
 echo "ya puedes desplegar en web localhost:9999"
 echo "fin"
